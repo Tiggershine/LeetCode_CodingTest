0a6f07534147e7116a2dab97655ea8413494d52a
Hyun Kim <kmasterh@gmail.com>
2024-05-16 12:56:23 +0200

    git init

---
+++ LeetCode/src/HashTable/Valid Anagram.java
@@ -0,0 +1,27 @@
+package HashTable;
+
+import java.util.*;
+
+class Solution {
+  public boolean isAnagram(String s, String t) {
+      if (s.length() != t.length()) { return false; }
+
+      Map<Character, Integer> countMap = new HashMap<>();
+      
+      for (char c: s.toCharArray()) {
+          countMap.put(c, countMap.getOrDefault(c, 0) + 1);
+      }
+
+      for (char c: t.toCharArray()) {
+          if (!countMap.containsKey(c)) {
+              return false;
+          }
+          countMap.put(c, countMap.getOrDefault(c, 0) - 1);
+          if (countMap.get(c) == 0) {
+              countMap.remove(c);
+          }
+      }
+
+      return countMap.isEmpty();
+  }
+}
